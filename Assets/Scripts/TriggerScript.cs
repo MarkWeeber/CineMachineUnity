6dd64fe6cd9a835edb89cad1e5cb6e42b9d3f26c
MarkWeeber <61727020+MarkWeeber@users.noreply.github.com>
2022-04-24 22:37:47 +0600

    postprocessing hw seems complete

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -9,10 +9,12 @@ public class TriggerScript : MonoBehaviour
     private LayerMask targetMask = 0;
     [SerializeField]
     private PlayableDirector timeline;
+    private bool activated = false;
     void OnTriggerEnter(Collider other)
     {
-        if ((targetMask.value & (1 << other.transform.gameObject.layer)) > 0)
+        if (((targetMask.value & (1 << other.transform.gameObject.layer)) > 0) && !activated)
         {
+            activated = true;
             if(timeline != null)
             {
                 timeline.Play();
